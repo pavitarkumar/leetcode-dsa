2b4e159eb19c162bf21f0ff6066bde7bce0d1603
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-07-28 00:32:44 +0530

    Time: 0 ms (100.00%) | Memory: 71.7 MB (53.08%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int ans = nums[0];
+        int fans = nums[0];
+        
+        for(int i = 1;i < nums.size() ;i++){
+            ans = max(nums[i] , ans + nums[i]);
+            fans = max(fans,ans);
+        }
+        return fans;
+    }
+};
