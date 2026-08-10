ef9ef2730be9d6835aa2186912f1df1a2d741f82
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-08-10 19:40:09 +0530

    Time: 6 ms (29.06%) | Memory: 43.7 MB (15.74%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -2,9 +2,8 @@ class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
         Arrays.sort(citations);
-        int papers;
         for(int i = 0;i < n;i++){
-            papers = n - i;
+            int papers = n - i;
             if(citations[i] >= papers){
                 return papers;
             }
