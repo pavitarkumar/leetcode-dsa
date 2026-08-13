a8d6b649551cfb90bde0988dceb8ad13f2fcad34
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-08-13 21:16:41 +0530

    Time: 1 ms (89.40%) | Memory: 43.2 MB (61.95%) - LeetSync

--- 520-detect-capital/detect-capital.java
+++ 520-detect-capital/detect-capital.java
@@ -2,13 +2,11 @@ class Solution {
     public boolean detectCapitalUse(String word) {
         int n = word.length();
         int u = 0;
-        int l = 0;
         for(int i = 0;i < n;i++){
             if(Character.isUpperCase(word.charAt(i))) u++;
-            else l++;
         }
-        if(l == n || u == n) return true;
-        if(n > 1 && Character.isUpperCase(word.charAt(0)) && l == n - 1) return true;
+        if(u == 0 || u == n) return true;
+        if(n > 1 && Character.isUpperCase(word.charAt(0)) && n - u == n - 1) return true;
         return false;
     }
 }
