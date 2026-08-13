d0fd21cc7f87c9fd786e869775ada38723409adf
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-08-13 21:21:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (84.01%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // StringBuilder s = "";
+        // for(int i = 0;i < s.length ;i++){
+        //     s += 
+        // }
+        return s.toLowerCase();
+    }
+}
