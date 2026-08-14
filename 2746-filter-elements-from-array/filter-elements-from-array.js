d998724105a937ebd195958e56dadc735c5ab5c9
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-08-14 21:27:14 +0530

    Time: 47 ms (37.60%) | Memory: 53.7 MB (41.95%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,9 +4,10 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
+    let size = 0;
     let ans = [];
     for(let i = 0; i < arr.length ;i++){
-        if(fn(arr[i],i)) ans.push(arr[i]);
+        if(fn(arr[i],i)) ans[size++] = arr[i];
     }
     return ans;
 };
