0234d9f004bed32d74a358a372ef17d2ea542648
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-08-23 19:59:56 +0530

    Time: 8 ms (53.28%) | Memory: 47.1 MB (41.61%) - LeetSync

--- 2039-sum-game/sum-game.java
+++ 2039-sum-game/sum-game.java
@@ -4,17 +4,17 @@ class Solution {
         int leftSum = 0 , rightSum = 0;
         int leftQ = 0 , rightQ = 0;
         for(int i = 0;i < n/2;i++){
-            if(num.charAt(i) == '?'){
-                leftQ++;
-            }else{
+            if(num.charAt(i) != '?'){
                 leftSum += num.charAt(i) - '0';
+            }else{
+                leftQ++;
             }
         }
         for(int i = n/2;i < n;i++){
-            if(num.charAt(i) == '?'){
-                rightQ++;
-            }else{
+            if(num.charAt(i) != '?'){
                 rightSum += num.charAt(i) - '0';
+            }else{
+                rightQ++;
             }
         }
         return (leftSum - rightSum) * 2 != (rightQ - leftQ) * 9;
