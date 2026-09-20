0a453541815697363ed5951ff10afb3dc79ad086
pavitar kumar <pavitarrukhaya65@gmail.com>
2026-09-20 16:12:25 +0530

    Time: 8 ms (95.99%) | Memory: 49.1 MB (12.67%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -1,26 +1,33 @@
 import java.util.Arrays;
+
 class Solution {
+
     public void wiggleSort(int[] nums) {
+
         Arrays.sort(nums);
 
         int n = nums.length;
-        int arr[] = new int[n];
+        int[] arr = new int[n];
 
-        int hi = n-1;
-        int low = (n-1)/2;
+        int low = (n - 1) / 2;  // end of first half
+        int high = n - 1;       // end of second half
 
         int i = 0;
-        while(i < n){
+
+        while (i < n) {
+
             arr[i] = nums[low];
-            i++;
             low--;
-            if(i < n){
-            arr[i] = nums[hi];
             i++;
-            hi--;
+
+            if (i < n) {
+                arr[i] = nums[high];
+                high--;
+                i++;
             }
         }
-        for(int j = 0 ;j < n;j++){
+
+        for (int j = 0; j < n; j++) {
             nums[j] = arr[j];
         }
     }
